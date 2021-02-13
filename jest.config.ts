d23224fe22af38be8b4fc8c0413132786c5d6e3f
jj1uty <jj1uty@jarl.com>
2021-02-13 14:57:37 +0900

    Revert "ts-jest の設定もれ修正"

--- jest.config.ts
+++ jest.config.ts
@@ -171,9 +171,7 @@ export default {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-  transform: {
-    "^.+\\.ts$": "ts-jest"
-  },
+  // transform: undefined,
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
