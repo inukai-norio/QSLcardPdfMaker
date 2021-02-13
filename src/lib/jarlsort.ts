0fc8a0d49f016adb949ca77ab0e7c51f73405125
jj1uty <jj1uty@jarl.com>
2021-02-13 14:57:54 +0900

    ソートアルゴリズム実装

--- src/lib/jarlsort.ts
+++ src/lib/jarlsort.ts
@@ -1,27 +1,54 @@
-import { getAmateurRadioInfoByCallsign } from 'callsign';
+import { getAmateurRadioInfoByCallsign } from 'callsign/src/node';
 import { SimpleAdif } from 'adif-parser-ts';
+
+type records = {
+  [field: string]: string;
+};
+
+const compare = (a: string, b: string) => {
+  if (a < b) {
+    return -1;
+  }
+  if (a > b) {
+    return 1;
+  }
+  return 0;
+}
+
+const preset = (v: string) => {
+  // JA1BCD -> 1JABCD
+  if (v[2] === '0') {
+    // ascii 文字コードの '9' より大きければいいので、'A' にする
+    return 'A' + v.slice(0, 2) + v.slice(3);
+  }
+  return v[2] + v.slice(0, 2) + v.slice(3);
+};
+
+const jaSort = (data: records[]): records[] => {
+  // 'J'で始まる局とその他（'7' or '8'）に分ける。
+  const dataJ = data.filter((v) => v.call[0] === 'J');
+  const data7 = data.filter((v) => v.call[0] !== 'J');
+  const sortedDataJ = dataJ.sort((a, b) => {
+    
+    const atemp = preset(a.call);
+    const btemp = preset(b.call);
+
+    return compare(atemp, btemp);
+  });
+  const sortedData7 = data7.sort((a, b) => compare(a.call, b.call));
+  return sortedDataJ.concat(sortedData7);
+};
 
 export const jarlsort = (data: SimpleAdif): SimpleAdif => {
   if (data.records === undefined) {
     return data;
   }
-  data.records = [
-    { call:"JF1DPF" },
-    { call:"JH2FNN" },
-    { call:"JJ2IJU" },
-    { call:"JA3QNI" },
-    { call:"JA4VPS" },
-    { call:"JA7BFT" },
-    { call:"JA7FVT" },
-    { call:"JA7WXL" },
-    { call:"JE7GXQ" },
-    { call:"JR0NEA" },
-    { call:"7L1DST" },
-    { call:"DS5USH" },
-    { call:"HL3EMT" },
-    { call:"R0CCG" },
-    { call:"UA0LW" },
-    { call:"VR2XZK" },
-  ];
+  // 1. JA局とその他（外国局）で分ける。
+  const recordsJA = data.records.filter((v) => getAmateurRadioInfoByCallsign(v.call).area === 'Japan');
+  const recordsDX = data.records.filter((v) => getAmateurRadioInfoByCallsign(v.call).area !== 'Japan');
+  const sortedRecordsJA = jaSort(recordsJA);
+  const sortedRecordsDX = recordsDX.sort((a, b) => compare(a.call, b.call));
+  
+  data.records = sortedRecordsJA.concat(sortedRecordsDX);
   return data;
 }
