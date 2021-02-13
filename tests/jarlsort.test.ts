ddd8d1b7675687be92f32ff9387acef00a0e1c19
jj1uty <jj1uty@jarl.com>
2021-02-13 14:57:45 +0900

    テストコマンドの修正

--- tests/jarlsort.test.ts
+++ tests/jarlsort.test.ts
@@ -4,11 +4,14 @@ import { jarlsort } from '../src/lib/jarlsort';
 
 describe('jarlsort', () => {
   it('jarlsort', () => {
-    const adi = readDdifAndParse('tests/data/jarlsort/test.adi');
-    const expected = fs.readFileSync('tests/data/jarlsort/test.json', {
+    const adi = readDdifAndParse('./tests/data/jarlsort/test.adi');
+    const expected = fs.readFileSync('./tests/data/jarlsort/test.json', {
       encoding: 'utf8',
     });
-
-    expect(jarlsort(adi).records.map((v) => v.call)).toEqual(JSON.parse(expected));
+    const records = jarlsort(adi).records;
+    if (records === undefined) {
+      return expect(records).not.toBeUndefined();
+    } 
+    expect(records.map((v) => v.call)).toEqual(JSON.parse(expected));
   })
 });
