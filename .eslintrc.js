52f5cc68caf7e8bdb9f9e3f1ba1d38f489d44fbc
jj1uty <jj1uty@jarl.com>
2021-02-14 23:02:43 +0900

    prettier を使うようにする

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'airbnb-typescript/base',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
