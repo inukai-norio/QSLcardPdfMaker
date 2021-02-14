e3410bacccd3806039052d77fea5c2cc0f3509d5
jj1uty <jj1uty@jarl.com>
2021-02-14 22:38:20 +0900

    とりあえずの設定を追加

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-typescript/base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: 'tsconfig.json',
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {},
+};
