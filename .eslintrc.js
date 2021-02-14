65c8732661110a22bdd5387c4453b92175ea35c3
jj1uty <jj1uty@jarl.com>
2021-02-14 23:04:47 +0900

    lint コマンド追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'airbnb-typescript/base',
     'prettier',
+    'prettier/@typescript-eslint',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
