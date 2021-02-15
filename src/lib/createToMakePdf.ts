c6c968fcf825f487bdf65608053c4b895e7d89bc
jj1uty <jj1uty@jarl.com>
2021-02-15 22:33:37 +0900

    とりあえずデータを突っ込む

---
+++ src/lib/createToMakePdf.ts
@@ -0,0 +1,102 @@
+import { SimpleAdif } from "adif-parser-ts";
+export type makePdf = {
+  band: String,
+  call: String,
+  frequency: String,
+  mode: String,
+  date: String,
+  time: String,
+  rst_rcvd: String,
+  rst_sent: String,
+  power: String,
+  gridsquare: String,
+};
+
+export const createToMakePdf = (data: SimpleAdif): makePdf[] => {
+  return [
+    {
+      "band": "40M",
+      "call": "JH1BTS",
+      "frequency": "7.042606",
+      "mode": "FT8",
+      "date": "20200414",
+      "time": "062400",
+      "rst_rcvd": "-12",
+      "rst_sent": "-20",
+      "power": "50.0",
+      "gridsquare": "PM95"
+    },
+    {
+      "band": "40M",
+      "call": "JR1WRH",
+      "frequency": "7.042085",
+      "mode": "FT8",
+      "date": "20200628",
+      "time": "063400",
+      "rst_rcvd": "-14",
+      "rst_sent": "-13",
+      "power": "50.0",
+      "gridsquare": "QM06"
+    },
+    {
+      "band": "40M",
+      "call": "JA4AQS",
+      "frequency": "7.042345",
+      "mode": "FT8",
+      "date": "20200630",
+      "time": "101300",
+      "rst_rcvd": "-10",
+      "rst_sent": "-03",
+      "power": "50.0",
+      "gridsquare": "PM64"
+    },
+    {
+      "band": "40M",
+      "call": "JA4FDZ",
+      "frequency": "7.042625",
+      "mode": "FT8",
+      "date": "20200710",
+      "time": "222200",
+      "rst_rcvd": "+01",
+      "rst_sent": "-04",
+      "power": "50.0",
+      "gridsquare": "PM64"
+    },
+    {
+      "band": "40M",
+      "call": "JA7FVT",
+      "frequency": "7.042967",
+      "mode": "FT4",
+      "date": "20200414",
+      "time": "060900",
+      "rst_rcvd": "-02",
+      "rst_sent": "-07",
+      "power": "50.0",
+      "gridsquare": "QM08"
+    },
+    {
+      "band": "40M",
+      "call": "JA7FVT",
+      "frequency": "7.042967",
+      "mode": "FT4",
+      "date": "20200414",
+      "time": "060900",
+      "rst_rcvd": "-02",
+      "rst_sent": "-07",
+      "power": "50.0",
+      "gridsquare": "QM08"
+    },
+    {
+      "band": "40M",
+      "call": "JA8XCS",
+      "frequency": "7.042212",
+      "mode": "FT8",
+      "date": "20191229",
+      "time": "000900",
+      "rst_rcvd": "-22",
+      "rst_sent": "-20",
+      "power": "10.0",
+      "gridsquare": "QN02"
+    }
+  ];
+}
