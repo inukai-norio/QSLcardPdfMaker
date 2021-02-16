5987801dd22eb6170b1a5360c3b4549d61d37a24
jj1uty <jj1uty@jarl.com>
2021-02-16 21:48:51 +0900

    初期実装

--- src/lib/createToMakePdf.ts
+++ src/lib/createToMakePdf.ts
@@ -13,90 +13,21 @@ export type makePdf = {
 };
 
 export const createToMakePdf = (data: SimpleAdif): makePdf[] => {
-  return [
-    {
-      "band": "40M",
-      "call": "JH1BTS",
-      "frequency": "7.042606",
-      "mode": "FT8",
-      "date": "20200414",
-      "time": "062400",
-      "rst_rcvd": "-12",
-      "rst_sent": "-20",
-      "power": "50.0",
-      "gridsquare": "PM95"
-    },
-    {
-      "band": "40M",
-      "call": "JR1WRH",
-      "frequency": "7.042085",
-      "mode": "FT8",
-      "date": "20200628",
-      "time": "063400",
-      "rst_rcvd": "-14",
-      "rst_sent": "-13",
-      "power": "50.0",
-      "gridsquare": "QM06"
-    },
-    {
-      "band": "40M",
-      "call": "JA4AQS",
-      "frequency": "7.042345",
-      "mode": "FT8",
-      "date": "20200630",
-      "time": "101300",
-      "rst_rcvd": "-10",
-      "rst_sent": "-03",
-      "power": "50.0",
-      "gridsquare": "PM64"
-    },
-    {
-      "band": "40M",
-      "call": "JA4FDZ",
-      "frequency": "7.042625",
-      "mode": "FT8",
-      "date": "20200710",
-      "time": "222200",
-      "rst_rcvd": "+01",
-      "rst_sent": "-04",
-      "power": "50.0",
-      "gridsquare": "PM64"
-    },
-    {
-      "band": "40M",
-      "call": "JA7FVT",
-      "frequency": "7.042967",
-      "mode": "FT4",
-      "date": "20200414",
-      "time": "060900",
-      "rst_rcvd": "-02",
-      "rst_sent": "-07",
-      "power": "50.0",
-      "gridsquare": "QM08"
-    },
-    {
-      "band": "40M",
-      "call": "JA7FVT",
-      "frequency": "7.042967",
-      "mode": "FT4",
-      "date": "20200414",
-      "time": "060900",
-      "rst_rcvd": "-02",
-      "rst_sent": "-07",
-      "power": "50.0",
-      "gridsquare": "QM08"
-    },
-    {
-      "band": "40M",
-      "call": "JA8XCS",
-      "frequency": "7.042212",
-      "mode": "FT8",
-      "date": "20191229",
-      "time": "000900",
-      "rst_rcvd": "-22",
-      "rst_sent": "-20",
-      "power": "10.0",
-      "gridsquare": "QN02"
+  if (data.records === undefined) {
+    throw new Error('data is undifined');
+  }
+  return data.records.map((v) => {
+    return {
+      band: v.band,
+      call: v.call,
+      frequency: v.freq,
+      mode: v.mode === 'MFSK' ? v.submode : v.mode,
+      date: v.qso_date,
+      time: v.time_on,
+      rst_rcvd: v.rst_rcvd,
+      rst_sent: v.rst_sent,
+      power: v.tx_pwr,
+      gridsquare: v.gridsquare,
     }
-  ];
+  });
 }
