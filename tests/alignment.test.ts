a78485f77cd639718a5a30eecbca826e9cee74bc
jj1uty <jj1uty@jarl.com>
2021-02-23 00:01:35 +0900

    テストコード修正
    壊れないはず

--- tests/alignment.test.ts
+++ tests/alignment.test.ts
@@ -1,9 +1,14 @@
+import { PDFDocument, PDFFont, StandardFonts } from 'pdf-lib';
 import * as alignment from '../src/lib/pdf/alignment';
 
 describe('alignment', () => {
-
+  let testFont: PDFFont;
+  beforeAll(async () => {
+    const pdfDoc = await PDFDocument.create();
+    testFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+  })
   it('left bottom', () => {
-    expect(alignment.leftBottom({ text: 'aaa', font: '', size: 12, x: 24, y: 66 })).toEqual({ x: 24, y: 66 });
+    expect(alignment.leftBottom({ text: 'aaa', font: testFont, size: 12, x: 24, y: 66 })).toEqual({ x: 24, y: 66 });
   });
 });
   
