f8a35c12d36fd9050991b76d274319717acb4d43
jj1uty <jj1uty@jarl.com>
2021-02-22 21:55:37 +0900

    オブジェクトでも送れるようにする

--- tests/mm2pt.test.ts
+++ tests/mm2pt.test.ts
@@ -2,14 +2,14 @@ import pt from '../src/lib/pdf/pt';
 
 describe('mm2pt', () => {
   it('tp or mm to pt 1', () => {
-    expect(pt(5.5, 'pt')).toBe(5.5);
+    expect(pt({ num: 5.5, unit: 'pt' })).toBe(5.5);
   })
   it('tp or mm to pt 2', () => {
-    expect(pt(1, 'mm')).toBe(72/25.4);
+    expect(pt({ num: 1, unit: 'mm' })).toBe(72/25.4);
   })
   
   it('tp or mm to pt 3', () => {
-    expect(() => (pt(1, <any>'nn'))).toThrow('undefined unit');
+    expect(() => (pt(<any>{ num: 1, unit: 'nn' }))).toThrow('undefined unit');
   })
   
   it('tp or mm to pt 4', () => {
