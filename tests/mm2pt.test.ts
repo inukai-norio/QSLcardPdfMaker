7ceb05a69f8f6acc504b0adfeee3c30aa540eb38
jj1uty <jj1uty@jarl.com>
2021-02-22 21:21:41 +0900

    テスト追加

--- tests/mm2pt.test.ts
+++ tests/mm2pt.test.ts
@@ -8,4 +8,11 @@ describe('mm2pt', () => {
   it('1pt', () => {
     expect(pt.mm2pt(25.4/72)).toBe(1);
   })
+
+  it('tp to tp 12', () => {
+    expect(pt.pt2pt(12)).toBe(12);
+  })
+  it('tp to tp 5.5', () => {
+    expect(pt.pt2pt(5.5)).toBe(5.5);
+  })
 });
