8a993ea7872ab935698c339791184e4f51ded574
jj1uty <jj1uty@jarl.com>
2021-02-23 00:17:31 +0900

    テストが通った

--- src/lib/pdf/alignment.ts
+++ src/lib/pdf/alignment.ts
@@ -7,3 +7,15 @@ export const rightBottom = (data: any) => {
   const l = data.font.widthOfTextAtSize(data.text, data.size);
   return { x: data.x, y: data.y - l };
 }
+
+export const leftTop = (data: any) => {
+  const { x, y } = data;
+  const h = data.font.heightAtSize(data.size)
+  return { x: x - h, y };
+};
+
+export const rightTop = (data: any) => {
+  const l = data.font.widthOfTextAtSize(data.text, data.size);
+  const h = data.font.heightAtSize(data.size)
+  return { x: data.x - h, y: data.y - l };
+}
