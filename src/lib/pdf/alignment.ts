76edbe9d89a4646bc81e25dc4118de750e8f288d
jj1uty <jj1uty@jarl.com>
2021-02-23 00:21:48 +0900

    正しい形に修正

--- src/lib/pdf/alignment.ts
+++ src/lib/pdf/alignment.ts
@@ -1,21 +1,25 @@
 export const leftBottom = (data: any) => {
   const { x, y } = data;
+  const l = data.font.widthOfTextAtSize(data.text, data.size);
+  const h = data.font.heightAtSize(data.size)
   return { x, y };
 };
 
 export const rightBottom = (data: any) => {
   const l = data.font.widthOfTextAtSize(data.text, data.size);
-  return { x: data.x, y: data.y - l };
+  const h = data.font.heightAtSize(data.size)
+  return { x: data.x - l, y: data.y };
 }
 
 export const leftTop = (data: any) => {
   const { x, y } = data;
+  const l = data.font.widthOfTextAtSize(data.text, data.size);
   const h = data.font.heightAtSize(data.size)
-  return { x: x - h, y };
+  return { x, y: y - h };
 };
 
 export const rightTop = (data: any) => {
   const l = data.font.widthOfTextAtSize(data.text, data.size);
   const h = data.font.heightAtSize(data.size)
-  return { x: data.x - h, y: data.y - l };
+  return { x: data.x - l, y: data.y - h };
 }
