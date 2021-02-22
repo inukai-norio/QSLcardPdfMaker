98bbf311768b001abef380ce78d9a81ed349c37a
jj1uty <jj1uty@jarl.com>
2021-02-22 22:02:00 +0900

    オブジェクトでも問題ないように修正

--- src/lib/pdf/pt.ts
+++ src/lib/pdf/pt.ts
@@ -1,9 +1,17 @@
-export default (num: number, unit: 'pt' | 'mm' = 'pt') => {
-  if (unit === 'pt') {
-    return num;
+type ptuu = {
+  num: number,
+  unit: 'pt' | 'mm',
+}
+
+export default (data: number | ptuu) => {
+  if (typeof data === 'number') {
+    return data;
   }
-  if (unit === 'mm') {
-    return 72/25.4 * num;
+  if (data.unit === 'pt') {
+    return data.num;
+  }
+  if (data.unit === 'mm') {
+    return 72/25.4 * data.num;
   }
   throw new Error('undefined unit')
 }
