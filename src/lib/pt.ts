2719175df018f3e0e345dbb5cd1bbf73e57c287b
jj1uty <jj1uty@jarl.com>
2021-02-22 21:28:11 +0900

    正しい実装にした

--- src/lib/pt.ts
+++ src/lib/pt.ts
@@ -1,2 +1,11 @@
 export const mm2pt = (mm: number) => 72/25.4 * mm;
 export const pt2pt = (pt: number) => pt;
+
+export const ptormm2pt = (data: any) => {
+  if (data.unit === 'pt') {
+    return pt2pt(data.num);
+  }
+  if (data.unit === 'mm') {
+    return mm2pt(data.num);
+  }
+}
