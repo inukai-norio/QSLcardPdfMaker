9a8dd6e77b693a2a006c8cb9ec2a31e0b1decac9
inukai-norio <inukai@sencorp.co.jp>
2021-02-22 22:27:46 +0900

    mm と pt に変更する関数作成 (#8)
    * pdf-lib を試す
    
    * 想定通りのコードになった

---
+++ src/lib/pdf/pt.ts
@@ -0,0 +1,28 @@
+type ptuu = {
+  num: number,
+  unit: 'pt' | 'mm',
+}
+
+export default (data: number | ptuu | string) => {
+  if (typeof data === 'number') {
+    return data;
+  }
+  if (typeof data === 'string') {
+    const num = parseFloat(data);
+    const unit = data.slice(-2);
+    if (unit === 'pt') {
+      return num;
+    }
+    if (unit === 'mm') {
+      return 72/25.4 * num;
+    }
+    throw new Error('undefined unit')
+  }
+  if (data.unit === 'pt') {
+    return data.num;
+  }
+  if (data.unit === 'mm') {
+    return 72/25.4 * data.num;
+  }
+  throw new Error('undefined unit')
+}
---
+++ tests/mm2pt.test.ts
@@ -0,0 +1,26 @@
+import pt from '../src/lib/pdf/pt';
+
+describe('mm2pt', () => {
+  it('tp or mm to pt 1', () => {
+    expect(pt({ num: 5.5, unit: 'pt' })).toBe(5.5);
+  })
+  it('tp or mm to pt 2', () => {
+    expect(pt({ num: 1, unit: 'mm' })).toBe(72/25.4);
+  })
+  
+  it('tp or mm to pt 3', () => {
+    expect(() => (pt(<any>{ num: 1, unit: 'nn' }))).toThrow('undefined unit');
+  })
+  
+  it('tp or mm to pt 4', () => {
+    expect(pt(5.5)).toBe(5.5);
+  })
+  
+  it('mm to pt', () => {
+    expect(pt('1mm')).toBe(72/25.4);
+  })
+  
+  it('pt to pt', () => {
+    expect(pt('12.5pt')).toBe(12.5);
+  })
+});
