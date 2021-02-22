0989d56bf14da9428843068c79bf4f1eef57866c
jj1uty <jj1uty@jarl.com>
2021-02-22 21:40:44 +0900

    もっと単純にする

--- src/lib/pt.ts
+++ src/lib/pt.ts
@@ -1,16 +1,13 @@
-const mm2pt = (mm: number) => 72/25.4 * mm;
-const pt2pt = (pt: number) => pt;
-
 type ptuu = {
   num: number,
   unit: 'pt' | 'mm',
 }
 export default (data: ptuu) => {
   if (data.unit === 'pt') {
-    return pt2pt(data.num);
+    return data.num;
   }
   if (data.unit === 'mm') {
-    return mm2pt(data.num);
+    return 72/25.4 * data.num;
   }
   throw new Error('undefined unit')
 }
