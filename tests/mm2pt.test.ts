02bff2ae16360a63a1b3cdb8f322b7520dc054eb
jj1uty <jj1uty@jarl.com>
2021-02-22 21:42:17 +0900

    変数は2つなので単純にする

--- tests/mm2pt.test.ts
+++ tests/mm2pt.test.ts
@@ -2,13 +2,13 @@ import pt from '../src/lib/pt';
 
 describe('mm2pt', () => {
   it('tp or mm to pt 1', () => {
-    expect(pt({ num: 5.5, unit: 'pt' })).toBe(5.5);
+    expect(pt(5.5, 'pt')).toBe(5.5);
   })
   it('tp or mm to pt 2', () => {
-    expect(pt({ num: 1, unit: 'mm' })).toBe(72/25.4);
+    expect(pt(1, 'mm')).toBe(72/25.4);
   })
   
   it('tp or mm to pt 3', () => {
-    expect(() => (pt(<any>{ num: 1, unit: 'nn' }))).toThrow('undefined unit');
+    expect(() => (pt(1, 'nn'))).toThrow('undefined unit');
   })
 });
