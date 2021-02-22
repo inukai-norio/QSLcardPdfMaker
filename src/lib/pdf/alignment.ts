22f3289c689f30b4b7a88da0aabbeb1b4d20e25d
jj1uty <jj1uty@jarl.com>
2021-02-23 00:28:08 +0900

    タイプ追加

--- src/lib/pdf/alignment.ts
+++ src/lib/pdf/alignment.ts
@@ -1,24 +1,39 @@
-export const leftBottom = (data: any) => {
+import { PDFFont } from "pdf-lib";
+
+type TextData = {
+  text: string,
+  font: PDFFont,
+  size: number,
+  x: number,
+  y: number,
+}
+
+type Point = {
+  x: number,
+  y: number,
+}
+
+export const leftBottom = (data: TextData): Point => {
   const { x, y } = data;
   const l = data.font.widthOfTextAtSize(data.text, data.size);
   const h = data.font.heightAtSize(data.size)
   return { x, y };
 };
 
-export const rightBottom = (data: any) => {
+export const rightBottom = (data: TextData): Point => {
   const l = data.font.widthOfTextAtSize(data.text, data.size);
   const h = data.font.heightAtSize(data.size)
   return { x: data.x - l, y: data.y };
 }
 
-export const leftTop = (data: any) => {
+export const leftTop = (data: TextData): Point => {
   const { x, y } = data;
   const l = data.font.widthOfTextAtSize(data.text, data.size);
   const h = data.font.heightAtSize(data.size)
   return { x, y: y - h };
 };
 
-export const rightTop = (data: any) => {
+export const rightTop = (data: TextData): Point => {
   const l = data.font.widthOfTextAtSize(data.text, data.size);
   const h = data.font.heightAtSize(data.size)
   return { x: data.x - l, y: data.y - h };
