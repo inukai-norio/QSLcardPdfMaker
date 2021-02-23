dc95f8300534c574f0b79e7c1709f62646d8f5d9
jj1uty <jj1uty@jarl.com>
2021-02-23 11:44:32 +0900

    変数化

--- tests/page.test.ts
+++ tests/page.test.ts
@@ -4,15 +4,16 @@ import * as page from '../src/lib/pdf/page';
 describe('page', () => {
   let originPageMock: {[name: string]: jest.Mock};
   let originPage: PDFPage;
+  let pagePage: page.Page;
   beforeEach(async () => {
     originPageMock = {
       drawText: jest.fn((text: string, options?: PDFPageDrawTextOptions) => Object.assign(options, { text }))
     }
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     originPage = <any>originPageMock;
+    pagePage = new page.Page(originPage);
   });
   it('orgin', () => {
-    const a = new page.Page(originPage);
-    expect(a.orgin).toEqual(originPage);
+    expect(pagePage.orgin).toEqual(originPage);
   });
 });
