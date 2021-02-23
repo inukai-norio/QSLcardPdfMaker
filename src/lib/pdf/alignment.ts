2ec5167c86840c897ead06460f49872226a8a609
jj1uty <jj1uty@jarl.com>
2021-02-23 10:46:17 +0900

    lint fix

--- src/lib/pdf/alignment.ts
+++ src/lib/pdf/alignment.ts
@@ -1,59 +1,61 @@
-import { PDFFont } from "pdf-lib";
+import { PDFFont } from 'pdf-lib';
 
 type TextData = {
   alignment?: {
-    vertical?: 'bottom' | 'middle' | 'top',
-    horizontal?: 'left' | 'center' | 'right',
-  },
-  text: string,
-  font: PDFFont,
-  size: number,
-  x: number,
-  y: number,
-}
+    vertical?: 'bottom' | 'middle' | 'top';
+    horizontal?: 'left' | 'center' | 'right';
+  };
+  text: string;
+  font: PDFFont;
+  size: number;
+  x: number;
+  y: number;
+};
 
 type Point = {
-  x: number,
-  y: number,
-}
+  x: number;
+  y: number;
+};
 
 export const leftBottom = (data: TextData): Point => {
   const { x, y } = data;
-  const l = data.font.widthOfTextAtSize(data.text, data.size);
-  const h = data.font.heightAtSize(data.size)
   return { x, y };
 };
 
 export const rightBottom = (data: TextData): Point => {
   const l = data.font.widthOfTextAtSize(data.text, data.size);
-  const h = data.font.heightAtSize(data.size)
   return { x: data.x - l, y: data.y };
-}
+};
 
 export const leftTop = (data: TextData): Point => {
   const { x, y } = data;
-  const l = data.font.widthOfTextAtSize(data.text, data.size);
-  const h = data.font.heightAtSize(data.size)
+  const h = data.font.heightAtSize(data.size);
   return { x, y: y - h };
 };
 
 export const rightTop = (data: TextData): Point => {
   const l = data.font.widthOfTextAtSize(data.text, data.size);
-  const h = data.font.heightAtSize(data.size)
+  const h = data.font.heightAtSize(data.size);
   return { x: data.x - l, y: data.y - h };
-}
+};
 
 export const text = (data: TextData): Point => {
   const l = data.font.widthOfTextAtSize(data.text, data.size);
-  const h = data.font.heightAtSize(data.size)
+  const h = data.font.heightAtSize(data.size);
   if (data.alignment === undefined) {
     return { x: data.x, y: data.y };
   }
   if (typeof data.alignment !== 'object') {
-    throw new Error('alignment is not object')
+    throw new Error('alignment is not object');
   }
-  if (data.alignment.horizontal === undefined || data.alignment.horizontal === 'left') {
-    if (data.alignment.vertical === undefined || data.alignment.vertical === 'bottom') {
+  if (
+    data.alignment.horizontal === undefined ||
+    data.alignment.horizontal === 'left'
+  ) {
+    if (
+      data.alignment.vertical === undefined ||
+      data.alignment.vertical === 'bottom'
+    ) {
       return { x: data.x, y: data.y };
     }
     if (data.alignment.vertical === 'top') {
@@ -64,7 +66,10 @@ export const text = (data: TextData): Point => {
     }
   }
   if (data.alignment.horizontal === 'right') {
-    if (data.alignment.vertical === undefined || data.alignment.vertical === 'bottom') {
+    if (
+      data.alignment.vertical === undefined ||
+      data.alignment.vertical === 'bottom'
+    ) {
       return { x: data.x - l, y: data.y };
     }
     if (data.alignment.vertical === 'top') {
@@ -75,7 +80,10 @@ export const text = (data: TextData): Point => {
     }
   }
   if (data.alignment.horizontal === 'center') {
-    if (data.alignment.vertical === undefined || data.alignment.vertical === 'bottom') {
+    if (
+      data.alignment.vertical === undefined ||
+      data.alignment.vertical === 'bottom'
+    ) {
       return { x: data.x - l / 2, y: data.y };
     }
     if (data.alignment.vertical === 'top') {
@@ -86,4 +94,4 @@ export const text = (data: TextData): Point => {
     }
   }
   throw new Error('undefined');
-}
+};
--- tests/alignment.test.ts
+++ tests/alignment.test.ts
@@ -6,71 +6,234 @@ describe('text', () => {
   beforeAll(async () => {
     const pdfDoc = await PDFDocument.create();
     testFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
-  })
+  });
   it('left bottom', () => {
-    expect(alignment.text({ alignment: { vertical: 'bottom', horizontal: 'left' }, text: 'aaa', font: testFont, size: 12, x: 24, y: 66 })).toEqual({ x: 24, y: 66 });
+    expect(
+      alignment.text({
+        alignment: { vertical: 'bottom', horizontal: 'left' },
+        text: 'aaa',
+        font: testFont,
+        size: 12,
+        x: 24,
+        y: 66,
+      })
+    ).toEqual({ x: 24, y: 66 });
   });
   it('right bottom', () => {
-    expect(alignment.text({ alignment: { vertical: 'bottom', horizontal: 'right' }, text: 'aaa', font: testFont, size: 12, x: 24, y: 66 })).toEqual({ x: 3.983999999999998, y: 66 });
+    expect(
+      alignment.text({
+        alignment: { vertical: 'bottom', horizontal: 'right' },
+        text: 'aaa',
+        font: testFont,
+        size: 12,
+        x: 24,
+        y: 66,
+      })
+    ).toEqual({ x: 3.983999999999998, y: 66 });
   });
   it('left top', () => {
-    expect(alignment.text({ alignment: { vertical: 'top', horizontal: 'left' }, text: 'aaa', font: testFont, size: 12, x: 24, y: 66 })).toEqual({ x: 24, y: 54.9 });
+    expect(
+      alignment.text({
+        alignment: { vertical: 'top', horizontal: 'left' },
+        text: 'aaa',
+        font: testFont,
+        size: 12,
+        x: 24,
+        y: 66,
+      })
+    ).toEqual({ x: 24, y: 54.9 });
   });
   it('right top', () => {
-    expect(alignment.text({ alignment: { vertical: 'top', horizontal: 'right' }, text: 'aaa', font: testFont, size: 12, x: 24, y: 66 })).toEqual({ x: 3.983999999999998, y: 54.9 });
+    expect(
+      alignment.text({
+        alignment: { vertical: 'top', horizontal: 'right' },
+        text: 'aaa',
+        font: testFont,
+        size: 12,
+        x: 24,
+        y: 66,
+      })
+    ).toEqual({ x: 3.983999999999998, y: 54.9 });
   });
 
   it('center top', () => {
-    expect(alignment.text({ alignment: { vertical: 'top', horizontal: 'center' }, text: 'aaa', font: testFont, size: 12, x: 24, y: 66 })).toEqual({ x: 13.991999999999999, y: 54.9 });
+    expect(
+      alignment.text({
+        alignment: { vertical: 'top', horizontal: 'center' },
+        text: 'aaa',
+        font: testFont,
+        size: 12,
+        x: 24,
+        y: 66,
+      })
+    ).toEqual({ x: 13.991999999999999, y: 54.9 });
   });
   it('center bottom', () => {
-    expect(alignment.text({ alignment: { vertical: 'bottom', horizontal: 'center' }, text: 'aaa', font: testFont, size: 12, x: 24, y: 66 })).toEqual({ x: 13.991999999999999, y: 66 });
+    expect(
+      alignment.text({
+        alignment: { vertical: 'bottom', horizontal: 'center' },
+        text: 'aaa',
+        font: testFont,
+        size: 12,
+        x: 24,
+        y: 66,
+      })
+    ).toEqual({ x: 13.991999999999999, y: 66 });
   });
   it('left middle', () => {
-    expect(alignment.text({ alignment: { vertical: 'middle', horizontal: 'left' }, text: 'aaa', font: testFont, size: 12, x: 24, y: 66 })).toEqual({ x: 24, y: 60.45 });
+    expect(
+      alignment.text({
+        alignment: { vertical: 'middle', horizontal: 'left' },
+        text: 'aaa',
+        font: testFont,
+        size: 12,
+        x: 24,
+        y: 66,
+      })
+    ).toEqual({ x: 24, y: 60.45 });
   });
   it('right middle', () => {
-    expect(alignment.text({ alignment: { vertical: 'middle', horizontal: 'right' }, text: 'aaa', font: testFont, size: 12, x: 24, y: 66 })).toEqual({ x: 3.983999999999998, y: 60.45 });
+    expect(
+      alignment.text({
+        alignment: { vertical: 'middle', horizontal: 'right' },
+        text: 'aaa',
+        font: testFont,
+        size: 12,
+        x: 24,
+        y: 66,
+      })
+    ).toEqual({ x: 3.983999999999998, y: 60.45 });
   });
 
   it('center middle', () => {
-    expect(alignment.text({ alignment: { vertical: 'middle', horizontal: 'center' }, text: 'aaa', font: testFont, size: 12, x: 24, y: 66 })).toEqual({ x: 13.991999999999999, y: 60.45 });
+    expect(
+      alignment.text({
+        alignment: { vertical: 'middle', horizontal: 'center' },
+        text: 'aaa',
+        font: testFont,
+        size: 12,
+        x: 24,
+        y: 66,
+      })
+    ).toEqual({ x: 13.991999999999999, y: 60.45 });
   });
 
   it('undefined undefined', () => {
-    expect(alignment.text({ text: 'aaa', font: testFont, size: 12, x: 24, y: 66 })).toEqual({ x: 24, y: 66 });
+    expect(
+      alignment.text({ text: 'aaa', font: testFont, size: 12, x: 24, y: 66 })
+    ).toEqual({ x: 24, y: 66 });
   });
   it('left undefined', () => {
-    expect(alignment.text({ alignment: { horizontal: 'left' }, text: 'aaa', font: testFont, size: 12, x: 24, y: 66 })).toEqual({ x: 24, y: 66 });
+    expect(
+      alignment.text({
+        alignment: { horizontal: 'left' },
+        text: 'aaa',
+        font: testFont,
+        size: 12,
+        x: 24,
+        y: 66,
+      })
+    ).toEqual({ x: 24, y: 66 });
   });
   it('center undefined', () => {
-    expect(alignment.text({ alignment: { horizontal: 'center' }, text: 'aaa', font: testFont, size: 12, x: 24, y: 66 })).toEqual({ x: 13.991999999999999, y: 66 });
+    expect(
+      alignment.text({
+        alignment: { horizontal: 'center' },
+        text: 'aaa',
+        font: testFont,
+        size: 12,
+        x: 24,
+        y: 66,
+      })
+    ).toEqual({ x: 13.991999999999999, y: 66 });
   });
   it('right undefined', () => {
-    expect(alignment.text({ alignment: { horizontal: 'right' }, text: 'aaa', font: testFont, size: 12, x: 24, y: 66 })).toEqual({ x: 3.983999999999998, y: 66 });
+    expect(
+      alignment.text({
+        alignment: { horizontal: 'right' },
+        text: 'aaa',
+        font: testFont,
+        size: 12,
+        x: 24,
+        y: 66,
+      })
+    ).toEqual({ x: 3.983999999999998, y: 66 });
   });
   it('undefined top', () => {
-    expect(alignment.text({ alignment: { vertical: 'top' }, text: 'aaa', font: testFont, size: 12, x: 24, y: 66 })).toEqual({ x: 24, y: 54.9 });
+    expect(
+      alignment.text({
+        alignment: { vertical: 'top' },
+        text: 'aaa',
+        font: testFont,
+        size: 12,
+        x: 24,
+        y: 66,
+      })
+    ).toEqual({ x: 24, y: 54.9 });
   });
   it('undefined middle', () => {
-    expect(alignment.text({ alignment: { vertical: 'middle' }, text: 'aaa', font: testFont, size: 12, x: 24, y: 66 })).toEqual({ x: 24, y: 60.45 });
+    expect(
+      alignment.text({
+        alignment: { vertical: 'middle' },
+        text: 'aaa',
+        font: testFont,
+        size: 12,
+        x: 24,
+        y: 66,
+      })
+    ).toEqual({ x: 24, y: 60.45 });
   });
   it('undefined bottom', () => {
-    expect(alignment.text({ alignment: { vertical: 'bottom' }, text: 'aaa', font: testFont, size: 12, x: 24, y: 66 })).toEqual({ x: 24, y: 66 });
+    expect(
+      alignment.text({
+        alignment: { vertical: 'bottom' },
+        text: 'aaa',
+        font: testFont,
+        size: 12,
+        x: 24,
+        y: 66,
+      })
+    ).toEqual({ x: 24, y: 66 });
   });
-  
+
   it('hoge undefined', () => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    expect((() => alignment.text({ alignment: <any>{ horizontal: 'hoge' }, text: 'aaa', font: testFont, size: 12, x: 24, y: 66 }))).toThrow();
+    expect(() =>
+    alignment.text({
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        alignment: <any>{ horizontal: 'hoge' },
+        text: 'aaa',
+        font: testFont,
+        size: 12,
+        x: 24,
+        y: 66,
+      })
+    ).toThrow();
   });
   it('undefined hoge', () => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    expect((() => alignment.text({ alignment: <any>{ vertical: 'hoge' }, text: 'aaa', font: testFont, size: 12, x: 24, y: 66 }))).toThrow();
+    expect(() =>
+    alignment.text({
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        alignment: <any>{ vertical: 'hoge' },
+        text: 'aaa',
+        font: testFont,
+        size: 12,
+        x: 24,
+        y: 66,
+      })
+    ).toThrow();
   });
-  
+
   it('alignment is hoge', () => {
+    expect(() =>
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    expect((() => alignment.text(<any>{ alignment: 'hoge', text: 'aaa', font: testFont, size: 12, x: 24, y: 66 }))).toThrow();
+    alignment.text(<any>{
+        alignment: 'hoge',
+        text: 'aaa',
+        font: testFont,
+        size: 12,
+        x: 24,
+        y: 66,
+      })
+    ).toThrow();
   });
 });
-  
