686a43bbc32a808007efd028b607f594bf35d21c
jj1uty <jj1uty@jarl.com>
2021-02-23 11:40:39 +0900

    テストをモックにする

--- tests/page.test.ts
+++ tests/page.test.ts
@@ -1,12 +1,15 @@
-import { PDFDocument, PDFPage } from 'pdf-lib';
+import { PDFPage, PDFPageDrawTextOptions } from 'pdf-lib';
 import * as page from '../src/lib/pdf/page';
 
 describe('page', () => {
-  let pdfDoc: PDFDocument;
+  let originPageMock: {[name: string]: jest.Mock};
   let originPage: PDFPage;
-  beforeAll(async () => {
-    pdfDoc = await PDFDocument.create();
-    originPage = pdfDoc.addPage();
+  beforeEach(async () => {
+    originPageMock = {
+      drawText: jest.fn((text: string, options?: PDFPageDrawTextOptions) => Object.assign(options, { text }))
+    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    originPage = <any>originPageMock;
   });
   it('orgin', () => {
     const a = new page.Page(originPage);
