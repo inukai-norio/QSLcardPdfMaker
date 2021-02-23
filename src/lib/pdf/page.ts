e7ed3c6c28a38909c680bd17ad6124f4204e7323
jj1uty <jj1uty@jarl.com>
2021-02-23 12:53:27 +0900

    とりあえずの実装

--- src/lib/pdf/page.ts
+++ src/lib/pdf/page.ts
@@ -1,4 +1,4 @@
-import { PDFPage, PDFPageDrawTextOptions } from "pdf-lib";
+import { PDFFont, PDFPage, PDFPageDrawTextOptions } from "pdf-lib";
 import { Alignment, text as alignmentText } from "./alignment";
 
 
@@ -7,12 +7,26 @@ export const a = 1;
 export class Page {
   private originPage;
 
+  private font?: PDFFont;
+
+  private fontSize?: number;
+
   constructor(originPage: PDFPage) {
     this.originPage = originPage;
   }
 
   get orgin(): PDFPage {
     return this.originPage;
+  }
+
+  setFont(font: PDFFont): void {
+    this.font = font;
+    this.originPage.setFont(this.font);
+  }
+
+  setFontSize(size: number): void {
+    this.fontSize = size;
+    this.originPage.setFontSize(this.fontSize);
   }
 
   drawText(text: string, options?: PDFPageDrawTextOptions & { alignment?: Alignment }): void {
