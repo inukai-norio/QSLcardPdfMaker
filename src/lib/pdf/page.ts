d72ff7731dda27825a47dd8c52622ad38512f205
jj1uty <jj1uty@jarl.com>
2021-02-23 13:03:48 +0900

    グリーン

--- src/lib/pdf/page.ts
+++ src/lib/pdf/page.ts
@@ -31,11 +31,31 @@ export class Page {
 
   drawText(text: string, options?: PDFPageDrawTextOptions & { alignment?: Alignment }): void {
     if (options !== undefined && options.alignment !== undefined) {
+      let font: PDFFont;
+      if (options.font === undefined) {
+        if (this.font !== undefined) {
+          font = this.font;
+        }else {
+          throw new Error();
+        }
+      } else {
+        font = options.font;
+      }
+      let size: number;
+      if (options.size === undefined) {
+        if (this.fontSize !== undefined) {
+          size = this.fontSize;
+        }else {
+          throw new Error();
+        }
+      } else {
+        size = options.size;
+      }
       const a = alignmentText({
         alignment: options.alignment,
         text,
-        font: <any>options.font,
-        size: <any>options.size,
+        font: <any>font,
+        size: <any>size,
         x: <any>options.x,
         y: <any>options.y,
       })
