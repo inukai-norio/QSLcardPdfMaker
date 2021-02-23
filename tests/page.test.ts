a54f434eae49ecab61366bd5d3546fc59d6cc5cb
jj1uty <jj1uty@jarl.com>
2021-02-23 13:21:39 +0900

    場所移動

--- tests/page.test.ts
+++ tests/page.test.ts
@@ -4,6 +4,7 @@ import * as page from '../src/lib/pdf/page';
 let originPageMock: {[name: string]: jest.Mock};
 let originPage: PDFPage;
 let pagePage: page.Page;
+let testFont: PDFFont;
 beforeEach(async () => {
   originPageMock = {
     drawText: jest.fn((text: string, options?: PDFPageDrawTextOptions) => {
@@ -16,17 +17,16 @@ beforeEach(async () => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   originPage = <any>originPageMock;
   pagePage = new page.Page(originPage);
+  const pdfDoc = await PDFDocument.create();
+  testFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
 });
 
 it('orgin', () => {
   expect(pagePage.orgin).toEqual(originPage);
 });
 
-describe('drawText', () => {
-  let testFont: PDFFont;
+describe('drawText (setFont)', () => {
   beforeEach(async () => {
-    const pdfDoc = await PDFDocument.create();
-    testFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
     pagePage.setFont(testFont);
     pagePage.setFontSize(12);
   })
