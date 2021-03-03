888769b7a55fbc4de806fe5782ea9feb2f8ef0a4
jj1uty <jj1uty@jarl.com>
2021-03-03 22:38:21 +0900

    テストコード修正

--- tests/ttp.test.ts
+++ tests/ttp.test.ts
@@ -1,10 +1,9 @@
-import { PDFPageFix } from '../src/lib/pdf/page';
 import drawLine from '../src/lib/ttp/drawLine';
 
 describe('drawLine', () => {
   it('drawLine', () => {
     const a = drawLine({ "start": { "x": "25mm", "y": "70mm" },  "end": {"x": "137mm",  "y": "70mm"}});
-    expect(a).toEqual((page: PDFPageFix) => page.drawLine({ "start": { "x": "25mm", "y": "70mm" },  "end": {"x": "137mm",  "y": "70mm"}}));
+    expect(a.toString()).toMatch(/page.*page\.drawLine.*options/);
   });
 });
 
