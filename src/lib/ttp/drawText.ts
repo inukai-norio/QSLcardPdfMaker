9329c9f504781919fe9c63e66932774a86b15298
jj1uty <jj1uty@jarl.com>
2021-03-12 22:00:11 +0900

    グリーン

--- src/lib/ttp/drawText.ts
+++ src/lib/ttp/drawText.ts
@@ -6,13 +6,26 @@ type FontString = {
 }
 type PDFPageDrawTextOptionsFixWithoutObject = Omit<PDFPageDrawTextOptionsFix, keyof FontString> & FontString
 
-export type PDFPageDrawTextOptionsFixWithText = { text: string, options?: PDFPageDrawTextOptionsFixWithoutObject };
+export type PDFPageDrawTextOptionsFixWithText = { text: string | Object, options?: PDFPageDrawTextOptionsFixWithoutObject };
 
-export default (o: PDFPageDrawTextOptionsFixWithText, fonts : { [field: string]: PDFFont }): (page: PDFPageFix) => void => ((page: PDFPageFix) => {
-  const { text, options } = o;
-  if (options !== undefined && options.font !== undefined) {
-    const font = fonts[options.font];
-    Reflect.set(options, 'font', font)
+export default (o: PDFPageDrawTextOptionsFixWithText, fonts : { [field: string]: PDFFont }): (page: PDFPageFix, userdata?: any, recode?: any) => void => ((page: PDFPageFix, userdata?: any, recode?: any) => {
+  const drawText = (text: string, options?: PDFPageDrawTextOptionsFixWithoutObject) => {
+    if (options !== undefined && options.font !== undefined) {
+      const font = fonts[options.font];
+      Reflect.set(options, 'font', font)
+    }
+    return page.drawText(text, <PDFPageDrawTextOptionsFix>options);
   }
-  return page.drawText(text, <PDFPageDrawTextOptionsFix>options);
+  
+  const { text, options } = o;
+  if (typeof text !== 'string') {
+    const type = (<any>text).type.split('.');
+    if (type[0] === 'recode' ) {
+      if (type[1] === 'date') {
+        const m = recode.date.getUTCMonth() + 1;
+        return drawText(m < 10 ? '0' + <string>m : m, options);
+      }
+    }
+  }
+  return drawText(<string>text, options);
 });
