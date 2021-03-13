adf4a8081eec5dec0b0bbf9265f6b944cc826f8f
jj1uty <jj1uty@jarl.com>
2021-03-13 14:03:29 +0900

    テスト修正

--- tests/createToMakePdf.test.ts
+++ tests/createToMakePdf.test.ts
@@ -6,12 +6,16 @@ describe('createToMakePdf', () => {
   it('createToMakePdf', () => {
     const adi = readDdifAndParse('./tests/data/createToMakePdf/test.adi');
     const ret = createToMakePdf(adi);
-    expect(
-      ret.map((v) => {
-        Reflect.set(v, 'date', v.date.toISOString());
-        return v;
-      })
-    ).toEqual(expected);
+    expect(ret.map((v) => v.date.toISOString())).toEqual(expected.map((v) => v.date));
+    expect(ret.map((v) => v.band)).toEqual(expected.map((v) => v.band));
+    expect(ret.map((v) => v.call)).toEqual(expected.map((v) => v.call));
+    expect(ret.map((v) => v.frequency)).toEqual(expected.map((v) => v.frequency));
+    expect(ret.map((v) => v.mode)).toEqual(expected.map((v) => v.mode));
+    expect(ret.map((v) => v.rst_rcvd)).toEqual(expected.map((v) => v.rst_rcvd));
+    expect(ret.map((v) => v.rst_sent)).toEqual(expected.map((v) => v.rst_sent));
+    expect(ret.map((v) => v.power)).toEqual(expected.map((v) => v.power));
+    expect(ret.map((v) => v.gridsquare)).toEqual(expected.map((v) => v.gridsquare));
+
   });
   it('throw1', () => {
     const adi = {
