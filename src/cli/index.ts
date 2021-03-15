17b250885d98912ad3e1d25ed1b4168a63c0817a
jj1uty <jj1uty@jarl.com>
2021-03-15 21:05:48 +0900

    lint fix

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -9,21 +9,20 @@ import jarlsort from '../lib/jarlsort';
 import recordsProxy from '../lib/recordsProxy';
 
 type Template = {
-  template: DoOption[],
+  template: DoOption[];
   font: {
     [field: string]: string;
-  },
-  height:string | number | Ptuu,
-  width: string | number | Ptuu,
-  defaultFont: string,
-  defaultSize: string | number | Ptuu,
-  callBox: boolean,
-  callText: boolean,
-  jarlsort: boolean,
-}
+  };
+  height: string | number | Ptuu;
+  width: string | number | Ptuu;
+  defaultFont: string;
+  defaultSize: string | number | Ptuu;
+  callBox: boolean;
+  callText: boolean;
+  jarlsort: boolean;
+};
 
 const callText = (pagea: PDFPageFix, call: string, font: PDFFont) => {
-  
   call.split('').forEach((v, i) => {
     const l = call.length;
     pagea.drawText(v, {
@@ -34,7 +33,7 @@ const callText = (pagea: PDFPageFix, call: string, font: PDFFont) => {
       font,
     });
   });
-}
+};
 
 const callBox = (pagea: PDFPageFix, call: string) => {
   const l = call.length > 6 ? 8 : 6;
@@ -61,7 +60,7 @@ const callBox = (pagea: PDFPageFix, call: string) => {
       },
       color: rgb(1, 0, 0),
     });
-    
+
     pagea.drawLine({
       start: {
         x: { num: 20, unit: 'mm' },
@@ -85,47 +84,69 @@ const callBox = (pagea: PDFPageFix, call: string) => {
       color: rgb(1, 0, 0),
     });
   }
-}
+};
 
+const main = async (
+  adifFile: string,
+  userdataFile: string,
+  templateFile: string,
+  outFile: string
+) => {
+  const userdataJSON = await fs.readFile(userdataFile, { encoding: 'utf8' });
+  const userdata = <{ [field: string]: string }>JSON.parse(userdataJSON);
 
-const main = async (adifFile: string, userdataFile: string, templateFile: string, outFile: string) => {    
-  const userdataJSON = await fs.readFile(userdataFile, {encoding: 'utf8'});
-  const userdata = <{[field: string]: string}>JSON.parse(userdataJSON);
-  
-  const templateJSON = await fs.readFile(templateFile, {encoding: 'utf8'});
+  const templateJSON = await fs.readFile(templateFile, { encoding: 'utf8' });
   const template = <Template>JSON.parse(templateJSON);
-  
-  const recs = (() => template.jarlsort ? recordsProxy(jarlsort(adif(adifFile))) : recordsProxy(adif(adifFile)))();
+
+  const recs = (() =>
+    template.jarlsort
+      ? recordsProxy(jarlsort(adif(adifFile)))
+      : recordsProxy(adif(adifFile)))();
 
   // PDF Creation
   const pdfDoc = await PDFDocument.create();
   pdfDoc.registerFontkit(fontkit);
-  
-  const font: {[field: string]: PDFFont} = {};
-  await Promise.all(Object.entries(template.font).map(async (o) => pdfDoc.embedFont(await fs.readFile(o[1])).then((v) => { font[o[0]] = v })));
+
+  const font: { [field: string]: PDFFont } = {};
+  await Promise.all(
+    Object.entries(template.font).map(async (o) =>
+      pdfDoc.embedFont(await fs.readFile(o[1])).then((v) => {
+        font[o[0]] = v;
+      })
+    )
+  );
 
   const ttp = new TTP(font);
   const DoOptionJSON = JSON.stringify(template.template);
 
   recs.forEach((rec) => {
     const pagea = page(
-      pdfDoc.addPage(<[number, number]>[template.height, template.width].map((v) => pt(v)))
+      pdfDoc.addPage(
+        <[number, number]>[template.height, template.width].map((v) => pt(v))
+      )
     );
-    
+
     pagea.setFont(font[template.defaultFont]);
     pagea.setFontSize(template.defaultSize);
-    
+
     if (template.callText) {
       callText(pagea, rec.call, font.m1mr);
     }
     if (template.callBox) {
       callBox(pagea, rec.call);
     }
-    (<DoOption[]>JSON.parse(DoOptionJSON)).forEach((o) => ttp.do(o)(pagea, userdata, rec));
+    (<DoOption[]>JSON.parse(DoOptionJSON)).forEach((o) =>
+      ttp.do(o)(pagea, userdata, rec)
+    );
   });
 
   const pdfBytes = await pdfDoc.save();
   await fs.writeFile(outFile, pdfBytes);
 };
 
-main('./tests/data/jarlsort/test.adi', 'userdata.json', './template/standard.json', 'aaa.pdf');
+main(
+  './tests/data/jarlsort/test.adi',
+  'userdata.json',
+  './template/standard.json',
+  'aaa.pdf'
+);
